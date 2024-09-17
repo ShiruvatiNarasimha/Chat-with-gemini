df24d7e12587e3b392bfa2aa3db20baa24c5dad9
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-09-18 01:00:58 +0530

    Main card update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Sidebar/>
+      <Main />
     </>
   )
 }
