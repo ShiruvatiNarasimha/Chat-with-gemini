cf926269fcdd71b6b5a79cbddeecc80b91359827
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-09-19 20:07:44 +0530

    Add

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Main from './components/Main/Main'
-import Sidebar from './components/Sidebar/Sidebar'
+import Sidebar from './components/Sidebar/Sidebar.css'
 
 function App() {
   return (
