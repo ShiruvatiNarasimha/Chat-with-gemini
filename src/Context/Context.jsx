132614c3b94553e65e8708ba9b4107bc75eb08c2
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-09-19 18:57:26 +0530

    Add sidebar logic

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -14,14 +14,41 @@ const ContextProvider = (props) => {
     const [resultData, setResultData] =  useState("");
 
 
+    const delayPara =(index,nextword) =>{
+        setTimeout(function () {
+            setResultData(prev=>prev+nextword);
+        }, 75*index)
+    }
+
+    
+
     const onSent = async (prompt) => {
 
         setResultData("")
         setLoading(true)
         setShowResult(true)
         setRecentPrompt(input)
+        setPrevPrompts(prev=>[...prev,input])
       const responsse = await runChat(input)
-      setRecentPrompt(responsse)
+      let responseArray = responsse.split("");
+      let newResponse="" ;
+      for(let i= 0; i< responseArray.length; i++)
+      {
+        if(i === 0 || i%2 !== 1) {
+            newResponse += responseArray[i];
+        }
+        else{
+            newResponse +=responseArray[i];
+        }
+        
+      }
+      let newResponse2 = newResponse.split("*").join("")
+      let newResponseArray = newResponse2.split(" ");
+      for(let i=0; i<newResponseArray.length; i++)
+      {
+        const nextword = newResponseArray[i];
+        delayPara(i, nextword+ " ")
+      }
       setLoading(false)
       setInput("")
     }
