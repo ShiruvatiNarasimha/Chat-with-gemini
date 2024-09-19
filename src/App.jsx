b92112a845376717099c1db022683861af0f0546
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-09-19 20:01:11 +0530

    Normalize line endings

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Main from './components/Main/Main'
-import Sidebar from './components/Sidebar/sidebar'
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   return (
